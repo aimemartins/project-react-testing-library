94c269abd26a7de0e873a458993b6418f2cc0b15
AIMÊ <aimemartins@hotmail.com>
2022-10-25 18:13:59 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  test('3.1) Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos ', () => {
+    // Acessar
+    renderWithRouter(<FavoritePokemons />);
+    // Agir
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    // Aferir
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
