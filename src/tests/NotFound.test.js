32d07f8e70dbe6a9cf44f3a24e5a69dff189bde8
AIMÊ <aimemartins@hotmail.com>
2022-10-25 19:02:50 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('4. Teste o componente <NotFound.js />', () => {
+  test('4.1) Teste se a página contém um heading h2 com o texto Page requested not found: ', () => {
+    // Acessar
+    renderWithRouter(<NotFound />);
+    // Agir
+    const titleNotFound = screen.getByRole('heading', { name: 'Page requested not found', level: 2 });
+    // Aferir
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('4.2) Teste se a página mostra a imagem:', () => {
+    // Acessar
+    renderWithRouter(<NotFound />);
+    // Agir
+    const imgNotFound = screen.getByRole('img');
+    // Aferir
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
